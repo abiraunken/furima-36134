09bf64628784c793811c23dedd39ce7573497760
abiraunken <g.maeshun@gmail.com>
2021-08-01 12:27:33 +0900

    コミット

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,4 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
 lock '3.16.0'
 
